91cf61e24b18f04d3c375f75ec83e708516b2899
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 20:30:47 +0100

    ft: Comenzamos con objetos básicos

--- the_quest_sith_edition/objects.py
+++ the_quest_sith_edition/objects.py
@@ -1,3 +1,15 @@
 """
 Aquí crearemos las clases de los objetos, como los asteroides y la nave
 """
+import os
+import pygame as pg
+from pygame.sprite import Sprite
+
+from . import ANCHO_PANTALLA, ALTO_PANTALLA
+
+
+class TIE():
+    pass
+
+    def __init__(self):
+        pass    
