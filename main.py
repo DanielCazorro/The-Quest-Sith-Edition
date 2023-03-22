073d2b64f5458538c73aaff56b63a1c7a8509e9b
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 17:03:03 +0100

    ft: Estructura básica del main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from the_quest_sith_edition.game import The_Quest
+
+if __name__ == "__main__":
+    juego = The_Quest()
+    # TODO: Aquí creo que deberemos colocar la función para "empezar" el juego
