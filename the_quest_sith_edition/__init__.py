6624f62759c0caac87d5b0e0aebeee087a684b6a
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 17:04:46 +0100

    ft: Añadir variables globales (FPS, alto y ancho)

--- the_quest_sith_edition/__init__.py
+++ the_quest_sith_edition/__init__.py
@@ -1,3 +1,7 @@
 """
 Aquí va toda la información del juego. Variables como el tamaño de la pantalla, los colores de textos, FPS, tamaño de asteroides, vidas, etc.
 """
+ALTO_PANTALLA = 720
+ANCHO_PANTALLA = 1280
+
+FPS = 60
