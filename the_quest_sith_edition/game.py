6ecf07fd11eea836ac4f337bf0e514a4f8000823
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 17:03:57 +0100

    ft: Creamos la clase del juego

--- the_quest_sith_edition/game.py
+++ the_quest_sith_edition/game.py
@@ -1,3 +1,10 @@
 """
 Este será el archivo donde va el juego en sí, es decir la clase del juego, con su constructor y su función principal de juego
 """
+import pygame as pg
+
+
+class The_Quest:
+    def __init__(self):
+        print("Se inicia el juego")
+        pg.init()
