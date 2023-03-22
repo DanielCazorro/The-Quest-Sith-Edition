f5645a5b42338fab04a292d5277c1c97c26cb3b8
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 20:30:34 +0100

    ft: Cambiamos display por pantalla

--- the_quest_sith_edition/game.py
+++ the_quest_sith_edition/game.py
@@ -1,6 +1,7 @@
 """
 Este será el archivo donde va el juego en sí, es decir la clase del juego, con su constructor y su función principal de juego
 """
+import os
 import pygame as pg
 
 from the_quest_sith_edition import ANCHO_PANTALLA, ALTO_PANTALLA
@@ -11,7 +12,7 @@ class The_Quest:
     def __init__(self):
         print("Se inicia el juego")
         pg.init()
-        self.display = pg.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
+        self.pantalla = pg.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
         pg.display.set_caption("The Quest - Sith Edition")
 
     def jugando(self):
@@ -22,6 +23,6 @@ class The_Quest:
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     salir = True
-            pg.draw.rect(self.display, (255, 255, 255),
+            pg.draw.rect(self.pantalla, (255, 255, 255),
                          pg.Rect(30, 60, 50, 150))
             pg.display.flip()
