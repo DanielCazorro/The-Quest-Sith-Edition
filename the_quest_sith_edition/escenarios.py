8b3d60b4d66f357c8ff26e340d19f4e7d51e567b
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 17:07:03 +0100

    ft: Creamos plantilla básica de pantallas

--- the_quest_sith_edition/escenarios.py
+++ the_quest_sith_edition/escenarios.py
@@ -1,3 +1,19 @@
 """
 Aquí crearemos las diferentes pantallas, como la pantalla de inicio, la pantalla de juego, etc.
 """
+import os
+import pygame as pg
+
+from the_quest_sith_edition import ANCHO_PANTALLA, ALTO_PANTALLA
+
+
+class Pantalla:
+    pass
+
+
+class Pantalla_Inicio(Pantalla):
+    pass
+
+
+class Pantalla_Jugar(Pantalla):
+    pass
