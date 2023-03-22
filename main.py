ae249a2536e21946206fbd12a7db4c8116240845
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-22 17:08:41 +0100

    ft: Añadimos iniciador del bucle principal

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from the_quest_sith_edition.game import The_Quest
 
 if __name__ == "__main__":
     juego = The_Quest()
-    # TODO: Aquí creo que deberemos colocar la función para "empezar" el juego
+    juego.jugando()
