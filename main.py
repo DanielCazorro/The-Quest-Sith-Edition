6fd9799bd0f939a97c32e93b0448452eab3c47bf
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-23 13:47:12 +0100

    ft: Print sencillo para comprobar las dimensiones

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from the_quest_sith_edition.game import The_Quest
+from the_quest_sith_edition.game import The_Quest, ALTO_PANTALLA, ANCHO_PANTALLA
+
 
 if __name__ == "__main__":
+    print(
+        f"El tamaño de pantalla es {ALTO_PANTALLA} Alto x {ANCHO_PANTALLA} Ancho")
     juego = The_Quest()
     juego.jugando()
