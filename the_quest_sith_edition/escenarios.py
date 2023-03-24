3ee30086f7d4108b46f48162818979b4ea8dee9d
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-24 12:21:12 +0100

    ft: Pequeña estructura pantalla historia

--- the_quest_sith_edition/escenarios.py
+++ the_quest_sith_edition/escenarios.py
@@ -47,6 +47,7 @@ class Pantalla_Inicio(Pantalla):
             "resources", "images", "fondo_pantalla_inicio.jpg")
         self.pantalla_inicio = pg.image.load(imagen_inicio)
 
+        # FIXME: Cambiar el color del logo para que resalte con el fondo
         logo_sith = os.path.join("resources", "images", "logo_sith.png")
         self.logo_sith = pg.image.load(logo_sith)
 
@@ -95,7 +96,7 @@ class Pantalla_Inicio(Pantalla):
 
     def pintar_texto_inicio_controles(self):
 
-        mensaje = "Pulsa <i>\f para ver los controles"
+        mensaje = "Pulsa <i> para ver los controles"
         texto = self.extra_peque.render(mensaje, False, (COLOR_AMARILLO))
         anchura_texto = texto.get_width()
         pos_x = (ANCHO_PANTALLA - anchura_texto) / 40
@@ -107,6 +108,67 @@ class Pantalla_Inicio(Pantalla):
         pos_x = (ANCHO_PANTALLA - ancho_titulo) / 2
         pos_y = ALTO_PANTALLA/48
         self.pantalla.blit(self.logo_sith, (pos_x, pos_y))
+
+
+class Pantalla_Instrucciones(Pantalla):
+    pass
+    # TODO: Aquí intento pensar que al dejar pulsado el botón (mientras se pulsa) aparezcan las instrucciones, si fuera posible con fondo invisible
+
+    # FIXME: Preguntar si es mejor todas las fuentes colocarlas digamos en la pantalla principal y luego se hereda, para no hacer tanto copia-pega
+
+
+class Pantalla_Historia(Pantalla):
+
+    def __init__(self, pantalla: pg.Surface):
+        super().__init__(pantalla)
+
+        fuente_titulo = os.path.join(
+            "resources", "fonts", "fuente-titulo.ttf")
+        self.titulo = pg.font.Font(fuente_titulo, 25)
+
+        fuente_historia = os.path.join(
+            "resources", "fonts", "fuente-historia.ttf")
+        self.historia = pg.font.Font(fuente_historia, 20)
+
+        fuente_extra = os.path.join(
+            "resources", "fonts", "fuente-extra.ttf")
+        self.extra = pg.font.Font(fuente_extra, 37)
+
+        # FIXME: PREGUNTAR AQUÍ SI HAY ALGÚN MÉTODO PARA CAMBIAR EL TAMAÑO DE LA LETRA DESDE LA FUNCIÓN PARA NO TENER QUE COPIAR Y PEGAR VARIAS VECES (REPETIDO)
+        fuente_extra_peque = os.path.join(
+            "resources", "fonts", "fuente-extra.ttf")
+        self.extra_peque = pg.font.Font(fuente_extra_peque, 25)
+
+        imagen_historia = os.path.join(
+            "resources", "images", "fondo_pantalla_historia.jpg")
+        self.pantalla_historia = pg.image.load(imagen_historia)
+
+    def bucle_principal(self):
+        """
+        Devuelve True si hay que finalizar el programa
+        Devuelve False si hay que pasar a la siguiente escena
+        """
+        super().bucle_principal()
+        salir = False
+        while not salir:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    return True
+                if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                    salir = True
+            # self.pantalla.fill((99, 0, 0))
+            self.pintar_fondo()
+            # self.pintar_texto_iniciar()
+            # self.pintar_texto_inicio_controles()
+            # self.pintar_texto_inicio_historia()
+            # self.pintar_logo()
+            pg.display.flip()
+        return False
+
+    def pintar_fondo(self):
+        # FIXME: Aquí sería mejor poner la ruta de la imagen???? En lugar de ponerla como variable general en la clase?
+        self.pantalla.fill((0, 0, 99))
+        self.pantalla.blit(self.pantalla_historia, (0, 0))
 
 
 class Pantalla_Jugar(Pantalla):
