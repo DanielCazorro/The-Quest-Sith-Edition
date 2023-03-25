22ac27290380fe336e85fb4dadd6bbb0b9db44e2
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-25 17:09:21 +0100

    ft: Estructura básica de la nave

--- the_quest_sith_edition/objects.py
+++ the_quest_sith_edition/objects.py
@@ -8,8 +8,26 @@ from pygame.sprite import Sprite
 from . import ANCHO_PANTALLA, ALTO_PANTALLA
 
 
-class TIE():
-    pass
+class Nave():
+    """
+    Esta clase será para la nave principal
+    """
+
+    velocidad = 5
 
     def __init__(self):
-        pass    
+        super().__init__()
+        imagen_nave = os.path.join("resources", "images", "nave.png")
+        self.image = pg.image.load(imagen_nave)
+        self.rect = self.image.get_rect(ANCHO_PANTALLA/6, ALTO_PANTALLA/2)
+
+    def update(self):
+        teclas_pulsar = pg.KEYDOWN()
+        if teclas_pulsar[pg.K_UP]:
+            self.rect.y += self.velocidad
+            if self.rect.y > ALTO_PANTALLA:
+                self.rect.y = ALTO_PANTALLA
+        if teclas_pulsar[pg.K_UP]:
+            self.rect.y -= self.velocidad
+            if self.rect.y < 0:
+                self.rect.y = 0
