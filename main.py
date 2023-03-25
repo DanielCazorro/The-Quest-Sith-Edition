ad7609b3d82c07da85bcc5dd7a973c9a39b9f50e
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-25 17:17:17 +0100

    ft: Protoestructura nave y asteroides

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from the_quest_sith_edition.game import The_Quest, ALTO_PANTALLA, ANCHO_PANTALLA
+from the_quest_sith_edition.game import The_Quest
 
 
 if __name__ == "__main__":
-    print(
-        f"El tamaño de pantalla es {ALTO_PANTALLA} Alto x {ANCHO_PANTALLA} Ancho")
     juego = The_Quest()
     juego.jugando()
--- the_quest_sith_edition/objects.py
+++ the_quest_sith_edition/objects.py
@@ -5,21 +5,24 @@ import os
 import pygame as pg
 from pygame.sprite import Sprite
 
-from . import ANCHO_PANTALLA, ALTO_PANTALLA
+from . import ANCHO_PANTALLA, ALTO_PANTALLA, FPS
 
 
 class Nave():
     """
     Esta clase será para la nave principal
     """
-
+    margen = 10
     velocidad = 5
+
+    # TODO: Se puden hacer varias imágenes, para que parezca que tenga movimiento
 
     def __init__(self):
         super().__init__()
         imagen_nave = os.path.join("resources", "images", "nave.png")
         self.image = pg.image.load(imagen_nave)
-        self.rect = self.image.get_rect(ANCHO_PANTALLA/6, ALTO_PANTALLA/2)
+        self.rect = self.image.get_rect(
+            midbottom=(ANCHO_PANTALLA/6, ALTO_PANTALLA/2))
 
     def update(self):
         teclas_pulsar = pg.KEYDOWN()
@@ -31,3 +34,15 @@ class Nave():
             self.rect.y -= self.velocidad
             if self.rect.y < 0:
                 self.rect.y = 0
+
+
+class Asteroides(Sprite):
+
+    velocidad_asteroide = -5
+
+    def __init__(self, *groups: _Group) -> None:
+        super().__init__(*groups)
+
+    def hay_colision(self, otro):
+        if self.rect.colliderect(otro):
+            pass
