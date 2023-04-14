919a9bd7abcc834df0365b62238ea3a9857c750a
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-14 12:45:19 +0200

    ft: Preparamos base de datos clase

--- the_quest_sith_edition/records.py
+++ the_quest_sith_edition/records.py
@@ -1,3 +1,16 @@
 """
 Aquí irá la clase records, y toda su función de escritura en la base de datos
 """
+import os
+import pygame as pg
+import sqlite3
+
+
+class Base_Gestion:
+    def __init__(self):
+        pass
+
+    def conseguir_puntuacion(self):
+        consulta = "SELECT * FROM records ORDER BY puntos DESC LIMIT 5"
+
+        conectar_base = sqlite3.connect()
